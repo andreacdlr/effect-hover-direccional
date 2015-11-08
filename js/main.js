3c2c70c7410424fb5a381f978f4d7e42e610008e
andreacdlr <andrea.crucesdelaroca@gmail.com>
2015-11-08 18:01:21 -0500

    hover proyect

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	/* Escribe aquí tu código de js */
+
+	        $(function () {
+
+               // $(' #item-gallery > li ').hoverdir();
+
+               $(' #item-gallery > li ').each( function() {
+                 $(this).hoverdir();
+
+                 });
+
+            });
+});
+
+
+
+
+
+
+
+ 
